2e2687f40d08206719227edd5da4e396744c3501
faisal-hameed <faisal.hameed@trilogy.com>
2020-04-07 11:21:45 +0500

    slide to left/right

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -14,56 +14,37 @@ export const transAnimation = animation([
 
 export const slideInAnimation =
     trigger('routeAnimations', [
-        transition('* <=> *', [
-            style({ position: 'relative' }),
-            query(':enter, :leave', [
-                style({
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%'
-                })
-            ]),
-            // new view is entering from far left
-            query(':enter', [
-                style({ left: '-100%' })
-            ]),
-            // apply child animation before current view left
-            query(':leave', animateChild()),
-            // overlay old and new animations
-            group([
-                query(':leave', [
-                    animate('300ms ease-out', style({ left: '100%' }))
-                ]),
-                query(':enter', [
-                    animate('300ms ease-out', style({ left: '0%' }))
-                ])
-            ]),
-            // Trigger new child animations
-            query(':enter', animateChild()),
-        ]),
-        transition('* <=> FilterPage', [
-            style({ position: 'relative' }),
-            query(':enter, :leave', [
-                style({
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%'
-                })
-            ]),
-            query(':enter', [
-                style({ left: '-100%' })
-            ]),
-            query(':leave', animateChild()),
-            group([
-                query(':leave', [
-                    animate('200ms ease-out', style({ left: '100%' }))
-                ]),
-                query(':enter', [
-                    animate('300ms ease-out', style({ left: '0%' }))
-                ])
-            ]),
-            query(':enter', animateChild()),
-        ])
+        transition('* <=> *', slideTo('left'))
     ]);
+
+function slideTo(direction) {
+    const optional = { optional: true };
+    return [
+        query(':enter, :leave', [
+            style({
+                position: 'absolute',
+                top: 0,
+                [direction]: 0,
+                width: '100%'
+            })
+        ], optional),
+        query(':enter', [
+            style({ [direction]: '-100%' })
+        ]),
+        // apply child animation before current view left
+        query(':leave', animateChild()),
+        group([
+            query(':leave', [
+                animate('600ms ease', style({ [direction]: '100%' }))
+            ], optional),
+            query(':enter', [
+                animate('600ms ease', style({ [direction]: '0%' }))
+            ])
+        ]),
+        // Normalize the page style... Might not be necessary
+
+        // Required only if you have child animations on the page
+        // query(':leave', animateChild()),
+        // query(':enter', animateChild()),
+    ];
+}
